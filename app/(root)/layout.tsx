90347e28ddc0aa90d8464c7292ba3c874a277cc9
meluiz <me.luizfelipe@gmail.com>
2026-08-19 15:26:51 -0300

    feat(app): add root layout component with header and main content structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import { Suspense } from 'react';
+
+import { Controls, Visitors } from '@/modules/root';
+
+const Layout = async (props: LayoutProps<'/'>) => {
+  const { children } = props;
+
+  return (
+    <div className="relative flex min-h-fit w-full flex-1 flex-col">
+      <a
+        href="#__APP_MAIN"
+        className="sr-only focus-visible:not-sr-only focus-visible:absolute focus-visible:top-2 focus-visible:left-2 focus-visible:z-50 focus-visible:rounded-md focus-visible:border focus-visible:border-stroke-soft focus-visible:bg-surface focus-visible:px-4 focus-visible:py-2 focus-visible:font-mono focus-visible:font-semibold focus-visible:text-foreground focus-visible:text-xs focus-visible:uppercase"
+      >
+        Skip to content
+      </a>
+      <header
+        id="__APP_HEADER"
+        className="relative w-full border-stroke-soft border-b bg-surface text-foreground"
+      >
+        <div className="container max-w-2xl border-stroke-soft px-4 sm:px-6 md:border-x">
+          <div className="center-between flex min-h-18 gap-x-6">
+            <Suspense fallback={<Visitors.Loading />}>
+              <Visitors.Stats />
+            </Suspense>
+            <Controls />
+          </div>
+        </div>
+      </header>
+      <main
+        tabIndex={-1}
+        id="__APP_MAIN"
+        className="relative flex w-full flex-1 bg-surface text-foreground"
+      >
+        <div className="container max-w-2xl flex-1 border-stroke-soft px-4 sm:px-6 md:border-x">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
