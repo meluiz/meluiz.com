e372ebad144c5675ccb2b7e40beffce8728e0b1d
meluiz <me.luizfelipe@gmail.com>
2026-08-28 18:23:36 -0300

    refactor(page.tsx): simplify layout structure by moving flex styles to Page.Section component to reduce nesting and improve readability

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,18 +5,16 @@ import { about, avatar } from '@/utils/constants';
 const HomePage = () => {
   return (
     <Page.Root>
-      <Page.Section aria-labelledby="page-title">
-        <div className="flex flex-col gap-y-6">
-          <About.Header avatar={avatar} name="Luiz Felipe" role="Front-end Developer" />
-          <div className="relative block space-y-6">
-            {about.map((block) => (
-              <About.Section
-                key={block.title}
-                title={block.title}
-                paragraphs={block.paragraphs}
-              />
-            ))}
-          </div>
+      <Page.Section className="gap-y-6" aria-labelledby="page-title">
+        <About.Header avatar={avatar} name="Luiz Felipe" role="Front-end Developer" />
+        <div className="relative space-y-6">
+          {about.map((block) => (
+            <About.Section
+              key={block.title}
+              title={block.title}
+              paragraphs={block.paragraphs}
+            />
+          ))}
         </div>
       </Page.Section>
     </Page.Root>
